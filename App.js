89fe04705608355b50ae5137a387f1c6062452a3
OsamaHaroon53 <sonijaosama@gmail.com>
2019-05-07 13:56:46 +0500

    add header and resolved delete bug

--- App.js
+++ App.js
@@ -7,15 +7,16 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Header, Content, Footer, Text, ListItem, Icon, Input, Item, Left, Button, Right, Body, CheckBox } from 'native-base';
+import { StyleSheet, TouchableOpacity, View, TouchableHighlight, Alert } from 'react-native';
+import { Container, Header, Content, Footer, Text, ListItem, Icon, Input, Item, Left, Button, Right, Body, CheckBox, Title } from 'native-base';
 // import getTheme from './native-base-theme/components';
 // import material from './native-base-theme/variables/material';
 
 export default class App extends Component {
   state = {
     todos: [],
-    todo: ''
+    todo: '',
+    isSearch: false
   }
 
   changeTextHandler = text => {
@@ -33,23 +34,64 @@ export default class App extends Component {
 
   deleteTodo = index => {
     let { todos } = this.state;
-    this.setState({ todos: [...todo.slice(index, 1)] }, () => console.log(this.state.todos));
+    todos.splice(index, 1);
+    console.log(todos);
+    this.setState({ todos: todos }, () => console.log(this.state.todos));
   }
+
+  // showHiddenView = () => {
+  //   console.log('ok');
+  //   this.setState({todo: 'osama'});
+  // }
+
   render() {
     let { todos } = this.state;
     return (
       // <StyleProvider style={getTheme(material)}>
       <Container>
-        <Header />
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            {this.state.isSearch ?
+              <Item style={styles.todo}>
+                <Input value={this.state.todo} placeholder='Search' onChangeText={this.changeTextHandler} />
+                <Button onPress={this.addTodo} style={{ backgroundColor: "#FF9501", height: '100%' }}>
+                  <Icon active name="add" />
+                </Button>
+              </Item>
+              :
+              <Title>Todo App</Title>}
+          </Body>
+          <Right>
+            <Button hasText transparent>
+              <Icon name='search' />
+            </Button>
+          </Right>
+        </Header>
+
         <Content>
 
           {/* <Text> */}
           {todos.length ? this.state.todos.map((el, i) => {
-            return (<ListItem icon key={i}>
+            return (<ListItem icon key={i} style={{ marginLeft: 5 }}>
               <Left>
                 <Button style={{ backgroundColor: "#FF9501" }}>
                   <Icon active name="bookmark" />
                 </Button>
+                {/* <TouchableOpacity
+                  onLongPress={() => { console.log("onLongPress");this.showHiddenView(); }}
+                >
+                  <Text>gfgdfgdfgdfg</Text>
+                 </TouchableOpacity> */}
+                {/* <TouchableHighlight onPress={() => { console.log("onPress") }} onLongPress={() => { console.log("onLongPress");Alert.alert("on Long Press jam") }} underlayColor="white">
+                  <View style={styles.button}>
+                    <Text style={styles.buttonText}>Touchable with Long Press</Text>
+                  </View>
+                </TouchableHighlight> */}
               </Left>
               {/* <CheckBox checked={true} /> */}
               <Body>
@@ -57,7 +99,7 @@ export default class App extends Component {
                 <Text>{el}</Text>
               </Body>
               <Right>
-                <Button onPress={() => this.deleteTodo(i)} style={{ backgroundColor: "red" }}>
+                <Button onPress={() => this.deleteTodo(i)} style={{ backgroundColor: "red" }} activeOpacity={0.1}>
                   <Icon active name="close" />
                 </Button>
               </Right>
@@ -68,7 +110,6 @@ export default class App extends Component {
         <Footer style={styles.footer}>
           <Item style={styles.todo}>
             <Input value={this.state.todo} placeholder='Add todo' onChangeText={this.changeTextHandler} />
-            {/* <Icon style={styles.icon} active name='add' /> */}
             <Button onPress={this.addTodo} style={{ backgroundColor: "#FF9501", height: '100%' }}>
               <Icon active name="add" />
             </Button>
@@ -91,5 +132,11 @@ const styles = StyleSheet.create({
   icon: {
     // backgroundColor: 'blue',
     // color: 'white'
+  },
+  border: {
+    borderWidth: 1,
+    borderColor: 'red',
+    borderStyle: 'solid',
+    marginLeft: 5
   }
 });
